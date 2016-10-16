62ae22fdf9f1c4589d9a34cb3ef7890b4d7f139f
Fabian Goerge <s150957@student.dtu.dk>
2016-10-16 15:14:31 +0200

    chagned mutex address

--- mutex/src/ecore_App.c
+++ mutex/src/ecore_App.c
@@ -3,23 +3,28 @@
 #include "e-lib.h"
 
 #define ptr_adr_X  0x80803500
+#define mutex_adr  0x00004000
 
 
 int main(void) {
 
 	int *x;
-	x = (int *) ptr_adr_X;
 	e_mutex_t    mutex; // group lock mutex
+	mutex = (void *) mutex_adr;
+
 
 	e_mutex_lock(0,0,&mutex);
+	x = (int *) ptr_adr_X;	
 	*x = *x+1;
 	e_mutex_unlock(0,0,&mutex);
 
 	e_mutex_lock(0,0,&mutex);
+	x = (int *) ptr_adr_X;	
 	*x = *x+1;
 	e_mutex_unlock(0,0,&mutex);
 
 	e_mutex_lock(0,0,&mutex);
+	x = (int *) ptr_adr_X;		
 	*x = *x+1;
 	e_mutex_unlock(0,0,&mutex);
 
